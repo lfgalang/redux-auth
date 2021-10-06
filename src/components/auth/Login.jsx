247f3489546e1a17ddea3a1dcc32041f845296b0
lfgalang <galanpipe3@gmail.com>
2021-10-05 20:31:27 -0500

    Vamos a cambiar de rama a la principal

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,55 +1,54 @@
 import React from 'react'
 import Footer from '../layout/Footer'
 import Header from '../layout/Header'
+import "./login.css"
 
 function Login() {
     return (
         <div>
-      <Header/>
-
-      <div className="sign-in-main">
-        <div className="container d-flex">
-          <div className="sign-in-container py-5 m-auto border">
-            <div className="sign-in-header">
-              <h4 className="font-weight-bold">Login</h4>
-              <p className="sign-in-intro">
-                <span className="text-muted">New to Food Delivery App ? </span>
-                <span className="text-danger font-weight-bold">Sign Up</span>
-              </p>
-              <div className="login-social-media py-3">
-                <button className="btn btn-primary btn-block btn-sm">
-                  Continue with Google
-                </button>
-              </div>
+        <Header/>
+            <div className="fondo sign-in-main">
+                <div className="container d-flex">
+                <div className="sign-in-container py-5 m-auto border">
+                    <div className="sign-in-header">
+                    <h4 className="font-weight-bold">Login</h4>
+                    <p className="sign-in-intro">
+                        <span className="text-muted">New to Food Delivery App ? </span>
+                        <span className="text-danger font-weight-bold">Sign Up</span>
+                    </p>
+                    <div className="login-social-media py-3">
+                        <button className="btn btn-primary btn-block btn-sm">
+                        Continue with Google
+                        </button>
+                    </div>
+                    </div>
+                    <form>
+                    <div className="form-group">
+                        <label htmlFor="InputEmail">Email address</label>
+                        <input
+                        type="email"
+                        className="form-control form-control-sm"
+                        />
+                        <small id="emailHelp" className="form-text text-muted">
+                        We'll never share your email with anyone else.
+                        </small>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="InputPassword1">Password</label>
+                        <input
+                        type="password"
+                        className="form-control form-control-sm"
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-danger btn-sm">
+                        Submit
+                    </button>
+                    </form>
+                </div>
+                </div>
             </div>
-            <form>
-              <div className="form-group">
-                <label htmlFor="InputEmail">Email address</label>
-                <input
-                  type="email"
-                  className="form-control form-control-sm"
-                />
-                <small id="emailHelp" className="form-text text-muted">
-                  We'll never share your email with anyone else.
-                </small>
-              </div>
-              <div className="form-group">
-                <label htmlFor="InputPassword1">Password</label>
-                <input
-                  type="password"
-                  className="form-control form-control-sm"
-                />
-              </div>
-              <button type="submit" className="btn btn-danger btn-sm">
-                Submit
-              </button>
-            </form>
-          </div>
+            <Footer />
         </div>
-      </div>
-
-      <Footer />
-    </div>
     )
 }
 
